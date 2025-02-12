78451f3356d6c5dcbbbc479fd409f2894b753035
Ignacio Haffner <ignaciohaffner@gmail.com>
2025-02-12 15:55:20 -0300

    Refactor drag event handler in Biography component for improved readability

--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -127,7 +127,7 @@ const Biography = () => {
               drag="x"
               dragConstraints={{ left: 0, right: 0 }}
               dragElastic={1}
-              onDragEnd={(e, { offset, velocity }) => {
+              onDragEnd={(_, { offset, velocity }) => {
                 const swipe = swipePower(offset.x, velocity.x);
 
                 if (swipe < -swipeConfidenceThreshold) {
